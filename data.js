5bd4534c1ea9c622abffa15365e722e004bb489b
somaj <somajm@csedge.org>
2018-10-06 13:18:04 -0700

    updated data.js

--- data.js
+++ data.js
@@ -1,5 +1,12 @@
 // Example
-let message = "Hello World!";
+let message = "Hello BTS!";
 console.log(message);
 
 // Type your code below!
+let calories = 891;
+let panckaesEaten = 1;
+let batteryLife= 20%;
+// please let boolean be spookylean for halloween
+let =
+let =
+let = iHaveFriends=false
